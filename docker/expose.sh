760c4846335c04b8e2f0630a0709e9816a5dab09
Majid d <majidaldosari-github@yahoo.com>
2015-09-18 15:37:40 -0400

    why did i put that there??

--- docker/expose.sh
+++ docker/expose.sh
@@ -15,7 +15,6 @@ dad=${rdd}/`ls -d ./*${2}`
 cmd="${dad}/run.sh ${3}"
 ssh ${1} /bin/sh -ctl "${cmd}"  > /dev/null 
 ssh ${1} "/opt/bin/weave expose" > /dev/null
-ssh ${1} /opt/bin/weave ps $2
 echo `ssh ${1} /opt/bin/weave ps ${2} | \
       grep -o -e '[0-9]*.[0-9]*.[0-9]*.[0-9]*/' | sed 's/\///'`
 
