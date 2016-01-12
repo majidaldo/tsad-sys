cb82df2b315841818b55c4c0b2046f8466462a12
Majid d <majidaldosari-github@yahoo.com>
2016-01-12 00:06:52 -0500

    i thought this was getting imgs from the registry

--- docker/2222-mongo-express/run.sh
+++ docker/2222-mongo-express/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #set -e
 
-docker rm -f mongo-express
+docker rm -f registry:5000/mongo-express
 docker run -d  \
 	--name mongo-express \
 	--link mongodb:mongo \
 	-e ME_CONFIG_MONGODB_SERVER='mongodb' \
 	-p 127.0.0.1:8081:8081 \
-	mongo-express "$@"
+	registry:5000/mongo-express "$@"
